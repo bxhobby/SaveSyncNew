2bc53103feba7a0c82b58f4c6a569917fcbcdc6d
bxhobby <chockchai@outlook.com>
2023-05-09 19:13:43 +0700

    create form add new customer

---
+++ Models/Front/AddCustomer.cs
@@ -0,0 +1,16 @@
+﻿namespace SaveSyncNew.Models.Front
+{
+    public class AddCustomer
+    {
+        public string? Licensecode { get; set; }
+        public string? ShopCode { get; set; }
+        public string? ShopName { get; set; }
+        public string? CustomerName { get; set; }
+        public string? Address { get; set; }
+        public string? SubDistrict { get; set; }
+        public string? District { get; set; }
+        public string? Province { get; set; }
+        public string? PostalCode { get; set; }
+        public string? Phone { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+using Radzen;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<DialogService>();
 
 var app = builder.Build();
 
