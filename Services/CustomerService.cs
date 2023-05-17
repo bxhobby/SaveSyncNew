7e2b2df598f4fe453bc8b21d1be06869afd5c614
bxhobby <chockchai@outlook.com>
2023-05-17 18:04:50 +0700

    create table customer

--- Services/CustomerService.cs
+++ Services/CustomerService.cs
@@ -16,7 +16,7 @@ namespace SaveSyncNew.Services
             try
             {
                 Customer.RegisterDate = DateTime.Now;
-                DataContext.Customer!.Add(Customer);
+                DataContext.Customer.Add(Customer);
                 var a = DataContext.SaveChanges();
                 return "Success";
             }
@@ -24,6 +24,44 @@ namespace SaveSyncNew.Services
             {
                 return e.Message;
             }
+        }
+
+        public void DeleteCustomer(int CustomerId)
+        {
+            Customer? FindCustomer = DataContext.Customer.Find(CustomerId);
+            try
+            {
+                if (FindCustomer is not null)
+                {
+                    DataContext.Customer.Remove(FindCustomer);
+                    DataContext.SaveChanges();
+                }
+            }
+            catch { }
+        }
+
+        public List<Customer>? LoadAllCustomer()
+        {
+            try
+            {
+                return DataContext.Customer.ToList();
+            }
+            catch
+            {
+                return new List<Customer>
+                {
+                    new Customer
+                    {
+                        LicenseCode="NULL",
+                        ShopCode="NULL",
+                        ShopName="NULL",
+                        CustomerName="NULL",
+                        Province="NULL",
+                        Phone="NULL",
+                    }
+                };
+            }
+
         }
     }
 }
